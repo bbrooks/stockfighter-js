c0296539ea6bf8af6f2378bac27db7cc7c848862
Ben Brooks <ben@benbrooks.net>
2015-12-14 06:34:37 -0800

    port from node wrapper
    Port jenius’s node wrapper
    (https://github.com/jenius/stockfighter-node) to client-side using
    axios (https://github.com/mzabriskie/axios) for the http request
    library.

--- README.md
+++ README.md
@@ -1,3 +1,112 @@
 # Stockfighter JS
 
-A simple client-side JS client for ([stockfighter.io](https://starfighter.io)). Since the stockfighter API does not implement CORS, this project includes a simple node server that will proxy your client-sides AJAX calls and allow you to play the game from your favorite browser console.
+A simple client-side JS client for ([stockfighter.io](https://starfighter.io)) ported from ([jenius's stockfighter node api wrapper](https://github.com/jenius/stockfighter-node)). Props to ([jenius](https://github.com/jenius/)) for the great work on the code and the documentation!
+
+This project includes a simple node server that will proxy your client-sides AJAX calls and allow you to play the game from your favorite browser console.
+
+### Installation
+
+1. Install node
+2. Clone or download this repo. 
+3. In your terminal, `cd` into `stockfighter-js` and run `node server.js`
+4. Visit `127.0.0.1:8000` in your browser
+5. Open up the console and start firing ([commands](#usage)) at the stockfighter api!
+
+### Usage
+
+This library exposes a class which you must initialize with an API key in order to use, as such:
+
+```js
+var client = new ApiClient(your_api_key);
+```
+
+Once you have initialized the client, you can call any of the following methods to get back the API response. All responses are returned as promises, because I like promises and the are a much better way to handle asynchronous control flow than callbacks. If you don't use promises, this is ok, they are very easy to handle and can be treated pretty much the same as callbacks if you decide not to use their extra power. A simple example is given below:
+
+```js
+client.heartbeat()
+  .then((res) => { console.log(res) })
+  .catch((err) => { console.error(err) });
+```
+
+### Methods
+
+Any method that takes parameters only accepts an object with the options set as key/value pairs. All methods that take options objects are documented fully along with the option names, types, and descriptions below.
+
+#### client.heartbeat()
+Checks if the API is online. ([official docs](https://starfighter.readme.io/docs/heartbeat))
+
+#### client.venue(options)
+Checks if a stock exchange is online. ([official docs](https://starfighter.readme.io/docs/venue-healthcheck))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+
+#### client.venue_stocks(options)
+Returns a list of stocks offered by a stock exchange. ([official docs](https://starfighter.readme.io/docs/list-stocks-on-venue))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+
+#### client.orderbook(options)
+Gets the order book for a specific stock. ([official docs](https://starfighter.readme.io/docs/get-orderbook-for-stock))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+- **stock** (String): symbol for the stock
+
+#### client.buy(options)
+Purchases a specific stock. ([official docs](https://starfighter.readme.io/docs/place-new-order))
+
+##### Options
+- **account** (String): bank account used to purchase
+- **venue** (String): symbol for stock exchange
+- **stock** (String): symbol for the stock
+- **price** (Integer or String) desired price as decimal, ex. 50.42 == $50.42
+- **quantity** (Integer or String) number of shares to buy
+- **type** (String) type of order, can be 'limit', 'market', 'fill-or-kill', or 'immediate-or-cancel'. See [the docs](https://starfighter.readme.io/docs/place-new-order#order-types) for descriptions of each type.
+
+#### client.sell(options)
+Sell a specific stock. ([official docs](https://starfighter.readme.io/docs/place-new-order))
+
+##### Options
+- **account** (String): bank account used to purchase
+- **venue** (String): symbol for stock exchange
+- **stock** (String): symbol for the stock
+- **price** (Integer or String) desired price as decimal, ex. 50.42 == $50.42
+- **quantity** (Integer or String) number of shares to buy
+- **type** (String) type of order, can be 'limit', 'market', 'fill-or-kill', or 'immediate-or-cancel'. See [the docs](https://starfighter.readme.io/docs/place-new-order#order-types) for descriptions of each type.
+
+#### client.quote(opts)
+Get the most recent trading information for a stock. ([official docs](https://starfighter.readme.io/docs/a-quote-for-a-stock))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+- **stock** (String): symbol for the stock
+
+#### client.order_status(opts)
+Get the updated status of an existing order. ([official docs](https://starfighter.readme.io/docs/status-for-an-existing-order))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+- **stock** (String): symbol for the stock
+- **id** (Integer): id of the order
+
+#### client.cancel_order(opts)
+Cancel an existing order. ([official docs](https://starfighter.readme.io/docs/cancel-an-order))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+- **stock** (String): symbol for the stock
+- **id** (Integer): id of the order
+
+#### client.all_orders(opts)
+See all orders in a given account's history ([official docs](https://starfighter.readme.io/docs/status-for-all-orders) and [this too](https://starfighter.readme.io/docs/status-for-all-orders-in-a-stock))
+
+##### Options
+- **venue** (String): symbol for stock exchange
+- **account** (String): account to view orders for
+- **stock** (String): optional - show only orders for this specific stock
+
+### License and Contributing
+
+This project is licensed under MIT, and the full license can be found [here](LICENSE.md).
--- js/stockfighter-client.js
+++ js/stockfighter-client.js
@@ -3,43 +3,205 @@
 class ApiClient {
 
   constructor(apiKey){
+    
+    if(!window.axios) {
+      return console.warn('HALTING: axios.js HTTP library not found');
+    }
+
+    this.BASE_URL = '/ob/api/';
     this.apiKey = apiKey;
+
   }
 
-  _request(url, type, data, cb){
-    var req = new XMLHttpRequest();
-    req.responseType = "json";
+  /**
+   * Check whether the API is online
+   * @return {Object} response entity
+   */
+  heartbeat () {
+    return this.client({ path: 'heartbeat' })
+               .then(res => { return res.data })
+  }
 
-    if(cb) {
-      var decoratedCb = this._responseCbBuilder(cb)
-      req.addEventListener('load', decoratedCb);
+  /**
+   * Check whether a stock exchange is online
+   * @param {Object} opts
+   * @param {String} opts.venue - symbol for stock exchange
+   * @return {Object} response entity
+   */
+  venue_heartbeat (opts) {
+    return this.client({
+      path: `venues/${opts.venue}/heartbeat`
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Get all stocks in a specific exchange
+   * @param {Object} opts
+   * @param {String} opts.venue - symbol for stock exchange
+   * @return {Object} response entity
+   */
+  venue_stocks (opts) {
+    return this.client({
+      path: `venues/${opts.venue}/stocks`
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Get the orderbook for a specific stock
+   * @param {Object} opts
+   * @param {String} opts.venue - symbol for stock exchange
+   * @param {String} opts.stock - symbol for stock
+   * @return {Object} response entity
+   */
+  orderbook (opts) {
+    return this.client({
+      path: `venues/${opts.venue}/stocks/${opts.stock}`
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Buy a stock
+   * @param  {Object} opts
+   * @param  {String} opts.account - bank account used to purchase
+   * @param  {String} opts.venue - symbol for stock exchange
+   * @param  {String} opts.stock - symbol for stock to buy
+   * @param  {Integer|String} opts.price - Desired price, ex. 50.42 == $50.42
+   * @param  {Integer|String} opts.quantity - number of shares to buy
+   * @param  {String} opts.type - type of order, can be 'limit', 'market',
+   *                              'fill-or-kill', 'immediate-or-cancel'
+   * @return {Object} response entity
+   */
+  buy (opts) {
+    let options = {
+      account: opts.account,
+      direction: 'buy',
+      orderType: opts.type,
+      qty: opts.quantity
     }
 
-    req.open(type, url);
-    req.setRequestHeader('X-Starfighter-Authorization', this.apiKey);
-
-    if( type === "POST") {
-      req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    if (opts.price) {
+      options.price = parseInt(opts.price.toString().replace('.', ''), 10)
     }
 
-    req.send(JSON.stringify(data));
-  }
-  
-  get(url, cb) {
-    this._request(url, 'GET', null, cb);
-  }
-  
-  post(url, data, cb){
-    this._request(url, 'POST', data, cb);
-  }
-  
-  delete(url, cb){
-    this._request(url, 'DELETE', null, cb);
+    return this._order(opts.venue, opts.stock, options)
   }
 
-  _responseCbBuilder(cb) {
-    return function(e){
-      cb(e.target.response);
+  /**
+   * Sell a stock
+   * @param  {Object} opts
+   * @param  {String} opts.account - bank account used to purchase
+   * @param  {String} opts.venue - symbol for stock exchange
+   * @param  {String} opts.stock - symbol for stock to sell
+   * @param  {Integer|String} opts.price - Desired price, ex. 50.42 == $50.42
+   * @param  {Integer|String} opts.quantity - number of shares to sell
+   * @param  {String} opts.type - type of order, can be 'limit', 'market',
+   *                              'fill-or-kill', 'immediate-or-cancel'
+   * @return {Object} response entity
+   */
+  sell (opts) {
+    let options = {
+      account: opts.account,
+      direction: 'sell',
+      orderType: opts.type,
+      qty: opts.quantity
     }
+
+    if (opts.price) {
+      options.price = parseInt(opts.price.toString().replace('.', ''), 10)
+    }
+
+    return this._order(opts.venue, opts.stock, options)
   }
+
+  /**
+   * Order a stock
+   * @private
+   * @param {String} venue - symbol for stock exchange
+   * @param {String} stock - symbol for stock to order
+   * @param {Object} options - see buy and sell docs
+   * @return {Object} response entity
+   */
+  _order (venue, stock, opts) {
+    return this.client({
+      path: `venues/${venue}/stocks/${stock}/orders`,
+      method: 'post',
+      entity: opts
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Get a quote for a stock's price
+   * @param {String} venue - symbol for stock exchange
+   * @param {String} stock - symbol for stock to quote
+   * @return {Object} response entity
+   */
+  quote (opts) {
+    return this.client({
+      path: `venues/${opts.venue}/stocks/${opts.stock}/quote`
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Get an order's status
+   * @param {Object} opts
+   * @param {String} opts.venue - symbol for stock exchange
+   * @param {String} opts.stock - symbol for stock
+   * @param {String} opts.id - order id
+   * @return {Object} response entity
+   */
+  order_status (opts) {
+    return this.client({
+      path: `venues/${opts.venue}/stocks/${opts.stock}/orders/${opts.id}`
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Cancel an order
+   * @param {Object} opts
+   * @param {String} opts.venue - symbol for stock exchange
+   * @param {String} opts.stock - symbol for stock
+   * @param {String} opts.id - order id
+   * @return {Object} response entity
+   */
+  cancel_order (opts) {
+    return this.client({
+      path: `venues/${opts.venue}/stocks/${opts.stock}/orders/${opts.id}`,
+      method: 'delete'
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Get status for all orders
+   * @param {Object} opts
+   * @param {String} opts.venue - symbol for stock exchange
+   * @param {String} opts.account - account number
+   * @param {String} opts.stock - symbol for stock
+   * @return {Object} response entity
+   */
+  all_orders (opts) {
+    let pathConfig = opts.stock
+    ? `venues/${opts.venue}/accounts/${opts.account}/stocks/${opts.stock}/orders`
+    : `venues/${opts.venue}/accounts/${opts.account}/orders`
+
+    return this.client({
+      path: pathConfig
+    }).then(res => { return res.data })
+  }
+
+  /**
+   * Converts client request to axios request
+   * @param {Object} opts - see https://github.com/mzabriskie/axios#request-api
+   * @return {Object} promise
+   */
+  client (opts) {
+    opts.headers = {
+      'X-Starfighter-Authorization': this.apiKey,
+    };
+    opts.url = this.BASE_URL+opts.path;
+    opts.data = opts.entity;
+    opts.method = opts.method || 'GET';
+
+    return axios(opts);
+  }
+
 }
